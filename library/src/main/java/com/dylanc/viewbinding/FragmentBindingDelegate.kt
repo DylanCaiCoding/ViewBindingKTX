cfd6cefaeffab3b2ed81c3c03f655100d0c76046
Dylanc <caishenglang@foxmail.com>
2020-12-17 18:21:58 +0800

    Prepare for release 1.0.1

---
+++ library/src/main/java/com/dylanc/viewbinding/FragmentBindingDelegate.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020. Dylan Cai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("unused")
+
+package com.dylanc.viewbinding
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.viewbinding.ViewBinding
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+class FragmentBindingDelegate<VB : ViewBinding>(
+  private val clazz: Class<VB>
+) : ReadOnlyProperty<Fragment, VB> {
+
+  private var isInitialized = false
+  private var _binding: VB? = null
+  private val binding: VB get() = _binding!!
+
+  override fun getValue(thisRef: Fragment, property: KProperty<*>): VB {
+    if (!isInitialized) {
+      thisRef.viewLifecycleOwner.lifecycle.addObserver(object : LifecycleObserver {
+        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        fun onDestroyView() {
+          _binding = null
+        }
+      })
+      _binding = inflateBinding(clazz, thisRef.layoutInflater)
+      isInitialized = true
+    }
+    return binding
+  }
+}
--- library/src/main/java/com/dylanc/viewbinding/ViewBindingUtil.kt
+++ library/src/main/java/com/dylanc/viewbinding/ViewBindingUtil.kt
@@ -1,12 +1,28 @@
+/*
+ * Copyright (c) 2020. Dylan Cai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:Suppress("unused")
 @file:JvmName("ViewBindingUtil")
 
 package com.dylanc.viewbinding
 
+import android.app.Activity
 import android.app.Dialog
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.activity.ComponentActivity
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import java.lang.reflect.ParameterizedType
@@ -16,15 +32,14 @@ import java.lang.reflect.ParameterizedType
  * @author Dylan Cai
  */
 
-inline fun <reified VB : ViewBinding> ComponentActivity.inflate(): Lazy<VB> = lazy {
+inline fun <reified VB : ViewBinding> Activity.inflate() = lazy {
   inflateBinding<VB>(layoutInflater).apply { setContentView(root) }
 }
 
-inline fun <reified VB : ViewBinding> Fragment.inflate(): Lazy<VB> = lazy {
-  inflateBinding(layoutInflater)
-}
+inline fun <reified VB : ViewBinding> Fragment.inflate() =
+  FragmentBindingDelegate(VB::class.java)
 
-inline fun <reified VB : ViewBinding> Dialog.inflate(): Lazy<VB> = lazy {
+inline fun <reified VB : ViewBinding> Dialog.inflate() = lazy {
   inflateBinding<VB>(layoutInflater).apply { setContentView(root) }
 }
 
@@ -38,11 +53,20 @@ inline fun <reified VB : ViewBinding> inflateBinding(layoutInflater: LayoutInfla
 
 @JvmName("inflate")
 @Suppress("UNCHECKED_CAST")
-fun <VB : ViewBinding> inflateBinding(clazz: Class<VB>, layoutInflater: LayoutInflater, parent: ViewGroup, attachToParent: Boolean) =
+fun <VB : ViewBinding> inflateBinding(
+  clazz: Class<VB>,
+  layoutInflater: LayoutInflater,
+  parent: ViewGroup,
+  attachToParent: Boolean
+) =
   clazz.getMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java)
     .invoke(null, layoutInflater, parent, attachToParent) as VB
 
-inline fun <reified VB : ViewBinding> inflateBinding(layoutInflater: LayoutInflater, parent: ViewGroup, attachToParent: Boolean) =
+inline fun <reified VB : ViewBinding> inflateBinding(
+  layoutInflater: LayoutInflater,
+  parent: ViewGroup,
+  attachToParent: Boolean
+) =
   inflateBinding(VB::class.java, layoutInflater, parent, attachToParent)
 
 @JvmName("inflateWithGeneric")
@@ -50,7 +74,11 @@ fun <VB : ViewBinding> Any.inflateBindingWithGeneric(layoutInflater: LayoutInfla
   withGenericBindingClass(this) { inflateBinding(it, layoutInflater) }
 
 @JvmName("inflateWithGeneric")
-fun <VB : ViewBinding> Any.inflateBindingWithGeneric(layoutInflater: LayoutInflater, parent: ViewGroup, attachToParent: Boolean): VB =
+fun <VB : ViewBinding> Any.inflateBindingWithGeneric(
+  layoutInflater: LayoutInflater,
+  parent: ViewGroup,
+  attachToParent: Boolean
+): VB =
   withGenericBindingClass(this) { inflateBinding(it, layoutInflater, parent, attachToParent) }
 
 private fun <VB : ViewBinding> withGenericBindingClass(any: Any, block: (Class<VB>) -> VB): VB {
