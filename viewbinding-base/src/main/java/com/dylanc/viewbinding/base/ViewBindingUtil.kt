98e5c840392de928435bbb42485d32c5b20f30db
DylanCaiCoding <caishenglang@foxmail.com>
2021-05-23 23:02:37 +0800

    Support for DataBinding to automatically set the lifecycle owner

--- viewbinding-base/src/main/java/com/dylanc/viewbinding/base/ViewBindingUtil.kt
+++ viewbinding-base/src/main/java/com/dylanc/viewbinding/base/ViewBindingUtil.kt
@@ -22,21 +22,32 @@ package com.dylanc.viewbinding.base
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.ComponentActivity
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import java.lang.reflect.ParameterizedType
 
 
 @JvmName("inflateWithGeneric")
 fun <VB : ViewBinding> Any.inflateBindingWithGeneric(layoutInflater: LayoutInflater): VB =
-  withGenericBindingClass(this) { clazz ->
+  withGenericBindingClass<VB>(this) { clazz ->
     clazz.getMethod("inflate", LayoutInflater::class.java).invoke(null, layoutInflater) as VB
+  }.also { binding ->
+    if (this is ComponentActivity && binding is ViewDataBinding) {
+      binding.lifecycleOwner = this
+    }
   }
 
 @JvmName("inflateWithGeneric")
 fun <VB : ViewBinding> Any.inflateBindingWithGeneric(layoutInflater: LayoutInflater, parent: ViewGroup?, attachToParent: Boolean): VB =
-  withGenericBindingClass(this) { clazz ->
+  withGenericBindingClass<VB>(this) { clazz ->
     clazz.getMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java)
       .invoke(null, layoutInflater, parent, attachToParent) as VB
+  }.also { binding ->
+    if (this is Fragment && binding is ViewDataBinding) {
+      binding.lifecycleOwner = viewLifecycleOwner
+    }
   }
 
 @JvmName("inflateWithGeneric")
@@ -44,8 +55,12 @@ fun <VB : ViewBinding> Any.inflateBindingWithGeneric(parent: ViewGroup): VB =
   inflateBindingWithGeneric(LayoutInflater.from(parent.context), parent, false)
 
 fun <VB : ViewBinding> Any.bindViewWithGeneric(view: View): VB =
-  withGenericBindingClass(this) { clazz ->
+  withGenericBindingClass<VB>(this) { clazz ->
     clazz.getMethod("bind", LayoutInflater::class.java).invoke(null, view) as VB
+  }.also { binding ->
+    if (this is Fragment && binding is ViewDataBinding) {
+      binding.lifecycleOwner = viewLifecycleOwner
+    }
   }
 
 private fun <VB : ViewBinding> withGenericBindingClass(any: Any, block: (Class<VB>) -> VB): VB {
