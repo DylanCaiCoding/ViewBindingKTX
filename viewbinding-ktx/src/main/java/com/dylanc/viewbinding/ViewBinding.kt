fa93c539d5712210083c671c49e8635a906e4feb
Dylanc <caishenglang@foxmail.com>
2021-12-11 16:36:26 +0800

    Add TabLayout#doOnCustomTabSelected()

--- viewbinding-ktx/src/main/java/com/dylanc/viewbinding/ViewBinding.kt
+++ viewbinding-ktx/src/main/java/com/dylanc/viewbinding/ViewBinding.kt
@@ -25,27 +25,29 @@ import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.*
 import androidx.viewbinding.ViewBinding
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
+private var View.binding: Any?
+  get() = getTag(-1)
+  set(value) = setTag(-1, value)
 
 inline fun <reified VB : ViewBinding> ComponentActivity.binding() = lazy {
-  inflateBinding<VB>(layoutInflater).also {
-    setContentView(it.root)
-    if (this is ViewDataBinding) lifecycleOwner = this@binding
+  inflateBinding<VB>(layoutInflater).also { binding ->
+    setContentView(binding.root)
+    if (binding is ViewDataBinding) binding.lifecycleOwner = this
   }
 }
 
-inline fun <reified VB : ViewBinding> Fragment.binding() = FragmentBindingDelegate<VB> { requireView().bind() }
+inline fun <reified VB : ViewBinding> Fragment.binding() =
+  FragmentBindingDelegate<VB> { requireView().getBinding() }
 
 inline fun <reified VB : ViewBinding> Fragment.binding(method: Method) =
-  FragmentBindingDelegate<VB> { if (method == Method.BIND) requireView().bind() else inflateBinding(layoutInflater) }
+  FragmentBindingDelegate<VB> { if (method == Method.BIND) requireView().getBinding() else inflateBinding(layoutInflater) }
 
 inline fun <reified VB : ViewBinding> Dialog.binding() = lazy {
   inflateBinding<VB>(layoutInflater).also { setContentView(it.root) }
@@ -58,18 +60,34 @@ inline fun <reified VB : ViewBinding> ViewGroup.binding(attachToParent: Boolean 
 inline fun <reified VB : ViewBinding> ViewGroup.inflate() =
   inflateBinding<VB>(LayoutInflater.from(context), this, true)
 
-inline fun <reified VB : ViewBinding> TabLayout.Tab.setCustomView(onBindView: VB.() -> Unit) {
-  customView = inflateBinding<VB>(LayoutInflater.from(parent!!.context)).apply(onBindView).root
+inline fun <reified VB : ViewBinding> TabLayout.Tab.setCustomView(block: VB.() -> Unit) {
+  inflateBinding<VB>(LayoutInflater.from(parent!!.context)).apply(block).let { binding ->
+    customView = binding.root
+    customView?.tag = binding
+  }
 }
 
-inline fun <reified VB : ViewBinding> TabLayout.Tab.bindCustomView(onBindView: VB.() -> Unit) =
-  customView?.bind<VB>()?.run(onBindView)
+inline fun <reified VB : ViewBinding> TabLayout.doOnCustomTabSelected(
+  crossinline onTabUnselected: VB.(TabLayout.Tab) -> Unit = {},
+  crossinline onTabReselected: VB.(TabLayout.Tab) -> Unit = {},
+  crossinline onTabSelected: VB.(TabLayout.Tab) -> Unit = {},
+) =
+  addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+    override fun onTabSelected(tab: TabLayout.Tab) {
+      tab.customView?.getBinding<VB>()?.onTabSelected(tab)
+    }
 
-inline fun <reified VB : ViewBinding> TabLayout.Tab.bindCustomView(bind: (View) -> VB, onBindView: VB.() -> Unit) =
-  customView?.let { bind(it) }?.run(onBindView)
+    override fun onTabUnselected(tab: TabLayout.Tab) {
+      tab.customView?.getBinding<VB>()?.onTabUnselected(tab)
+    }
 
-inline fun <reified VB : ViewBinding> NavigationView.setHeaderView(index: Int = 0, onBindView: VB.() -> Unit) =
-  getHeaderView(index)?.bind<VB>()?.run(onBindView)
+    override fun onTabReselected(tab: TabLayout.Tab) {
+      tab.customView?.getBinding<VB>()?.onTabReselected(tab)
+    }
+  })
+
+inline fun <reified VB : ViewBinding> NavigationView.setHeaderView(index: Int = 0, block: VB.() -> Unit) =
+  getHeaderView(index)?.getBinding<VB>()?.run(block)
 
 inline fun <reified VB : ViewBinding> inflateBinding(layoutInflater: LayoutInflater) =
   VB::class.java.getMethod("inflate", LayoutInflater::class.java).invoke(null, layoutInflater) as VB
@@ -83,16 +101,15 @@ inline fun <reified VB : ViewBinding> inflateBinding(
   VB::class.java.getMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java)
     .invoke(null, layoutInflater, parent, attachToParent) as VB
 
-inline fun <reified VB : ViewBinding> View.bind() =
-  VB::class.java.getMethod("bind", View::class.java).invoke(null, this) as VB
+inline fun <reified VB : ViewBinding> View.getBinding() = getBinding(VB::class.java)
 
-inline fun Fragment.doOnDestroyView(crossinline block: () -> Unit) =
-  viewLifecycleOwner.lifecycle.addObserver(object : LifecycleObserver {
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    fun onDestroyView() {
-      block.invoke()
-    }
-  })
+@Suppress("UNCHECKED_CAST")
+fun <VB : ViewBinding> View.getBinding(clazz: Class<VB>) =
+  if (binding != null) {
+    binding as VB
+  } else {
+    (clazz.getMethod("bind", View::class.java).invoke(null, this) as VB).also { binding = it }
+  }
 
 enum class Method { BIND, INFLATE }
 
@@ -106,16 +123,18 @@ class FragmentBindingDelegate<VB : ViewBinding>(private val block: () -> VB) : R
   override fun getValue(thisRef: Fragment, property: KProperty<*>): VB {
     if (binding == null) {
       binding = try {
-        block().also {
-          if (it is ViewDataBinding) it.lifecycleOwner = thisRef.viewLifecycleOwner
+        block().also { binding ->
+          if (binding is ViewDataBinding) binding.lifecycleOwner = thisRef.viewLifecycleOwner
         }
       } catch (e: IllegalStateException) {
         throw IllegalStateException("The binding property has been destroyed.")
       }
-      thisRef.doOnDestroyView {
-        if (thisRef is BindingLifecycleOwner) thisRef.onDestroyViewBinding(binding!!)
-        binding = null
-      }
+      thisRef.viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+        override fun onDestroy(owner: LifecycleOwner) {
+          if (thisRef is BindingLifecycleOwner) thisRef.onDestroyViewBinding(binding!!)
+          binding = null
+        }
+      })
     }
     return binding!!
   }
--- viewbinding-nonreflection-ktx/src/main/java/com/dylanc/viewbinding/nonreflection/ViewBinding.kt
+++ viewbinding-nonreflection-ktx/src/main/java/com/dylanc/viewbinding/nonreflection/ViewBinding.kt
@@ -25,9 +25,7 @@ import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.*
 import androidx.viewbinding.ViewBinding
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
@@ -36,9 +34,9 @@ import kotlin.reflect.KProperty
 
 
 fun <VB : ViewBinding> ComponentActivity.binding(inflate: (LayoutInflater) -> VB) = lazy {
-  inflate(layoutInflater).also {
-    setContentView(it.root)
-    if (this is ViewDataBinding) lifecycleOwner = this@binding
+  inflate(layoutInflater).also { binding ->
+    setContentView(binding.root)
+    if (binding is ViewDataBinding) binding.lifecycleOwner = this
   }
 }
 
@@ -55,29 +53,39 @@ fun <VB : ViewBinding> ViewGroup.binding(
   inflate(LayoutInflater.from(context), if (attachToParent) this else null, attachToParent)
 }
 
-inline fun <reified VB : ViewBinding> ViewGroup.inflate(inflate: (LayoutInflater, ViewGroup?, Boolean) -> VB) =
+fun <VB : ViewBinding> ViewGroup.inflate(inflate: (LayoutInflater, ViewGroup?, Boolean) -> VB) =
   inflate(LayoutInflater.from(context), this, true)
 
-fun <VB : ViewBinding> TabLayout.Tab.setCustomView(
-  inflate: (LayoutInflater) -> VB,
-  onBindView: VB.() -> Unit
-) {
-  customView = inflate(LayoutInflater.from(parent!!.context)).apply(onBindView).root
+fun <VB : ViewBinding> TabLayout.Tab.setCustomView(inflate: (LayoutInflater) -> VB, block: VB.() -> Unit) {
+  customView = inflate(LayoutInflater.from(parent!!.context)).apply(block).root
 }
 
-inline fun <reified VB : ViewBinding> TabLayout.Tab.bindCustomView(bind: (View) -> VB, onBindView: VB.() -> Unit) =
-  customView?.let { bind(it) }?.run(onBindView)
+fun <VB : ViewBinding> TabLayout.doOnCustomTabSelected(
+  bind: (View) -> VB,
+  onTabUnselected: (VB.(TabLayout.Tab) -> Unit)? = null,
+  onTabReselected: (VB.(TabLayout.Tab) -> Unit)? = null,
+  onTabSelected: (VB.(TabLayout.Tab) -> Unit)? = null,
+) =
+  addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+    override fun onTabSelected(tab: TabLayout.Tab) {
+      tab.getBinding(bind)?.let { onTabSelected?.invoke(it, tab) }
+    }
 
-inline fun <reified VB : ViewBinding> NavigationView.setHeaderView(index: Int = 0, bind: (View) -> VB, onBindView: VB.() -> Unit) =
-  getHeaderView(index)?.let { bind(it) }?.run(onBindView)
+    override fun onTabUnselected(tab: TabLayout.Tab) {
+      tab.getBinding(bind)?.let { onTabUnselected?.invoke(it, tab) }
+    }
 
-inline fun Fragment.doOnDestroyView(crossinline block: () -> Unit) =
-  viewLifecycleOwner.lifecycle.addObserver(object : LifecycleObserver {
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    fun onDestroyView() {
-      block.invoke()
+    override fun onTabReselected(tab: TabLayout.Tab) {
+      tab.getBinding(bind)?.let { onTabReselected?.invoke(it, tab) }
     }
   })
+
+@Suppress("UNCHECKED_CAST")
+fun <VB : ViewBinding> TabLayout.Tab.getBinding(bind: (View) -> VB): VB? =
+  (tag as? VB) ?: customView?.let { bind(it) }?.also { tag = it }
+
+fun <VB : ViewBinding> NavigationView.setHeaderView(index: Int = 0, bind: (View) -> VB, block: VB.() -> Unit) =
+  getHeaderView(index)?.let { bind(it) }?.run(block)
 
 interface BindingLifecycleOwner {
   fun onDestroyViewBinding(destroyingBinding: ViewBinding)
@@ -89,16 +97,18 @@ class FragmentBindingDelegate<VB : ViewBinding>(private val bind: (View) -> VB) 
   override fun getValue(thisRef: Fragment, property: KProperty<*>): VB {
     if (binding == null) {
       binding = try {
-        bind(thisRef.requireView()).also {
-          if (it is ViewDataBinding) it.lifecycleOwner = thisRef.viewLifecycleOwner
+        bind(thisRef.requireView()).also { binding ->
+          if (binding is ViewDataBinding) binding.lifecycleOwner = thisRef.viewLifecycleOwner
         }
       } catch (e: IllegalStateException) {
         throw IllegalStateException("The binding property has been destroyed.")
       }
-      thisRef.doOnDestroyView {
-        if (thisRef is BindingLifecycleOwner) thisRef.onDestroyViewBinding(binding!!)
-        binding = null
-      }
+      thisRef.viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+        override fun onDestroy(owner: LifecycleOwner) {
+          if (thisRef is BindingLifecycleOwner) thisRef.onDestroyViewBinding(binding!!)
+          binding = null
+        }
+      })
     }
     return binding!!
   }
